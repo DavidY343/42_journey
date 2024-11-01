72f7fa2a9ee2cd5422589d5e7bde45748bb1770a
david <tipiraenkewa@gmail.com>
2024-11-01 01:21:07 +0100

    error in main resolved

--- CPPs/2/ex01/main.cpp
+++ CPPs/2/ex01/main.cpp
@@ -6,28 +6,28 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:05:58 by david             #+#    #+#             */
-/*   Updated: 2024/11/01 01:09:52 by david            ###   ########.fr       */
+/*   Updated: 2024/11/01 01:20:48 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-#include <iostream>
-#include "Fixed.hpp"
 
-int main(void)
+int main( void )
 {
-    Fixed a;
-    Fixed const b(Fixed(5.05f) * Fixed(2));
-
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
-
-    return 0;
+Fixed a;
+Fixed const b( 10 );
+Fixed const c( 42.42f );
+Fixed const d( b );
+a = Fixed( 1234.4321f );
+std::cout << "a is " << a << std::endl;
+std::cout << "b is " << b << std::endl;
+std::cout << "c is " << c << std::endl;
+std::cout << "d is " << d << std::endl;
+std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+return 0;
 }
