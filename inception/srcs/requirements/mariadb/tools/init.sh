e154a0a5ab4da2ede2ff2edb07668491bcd7d5be
david <david@david.david>
2025-08-07 09:25:00 -0500

    ready to test

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Crear el socket directory si no existe
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 # Inicializar la base de datos si no existe
 if [ ! -d /var/lib/mysql/mysql ]; then
   echo "Initializing MariaDB data directory..."
@@ -10,9 +14,9 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   mysqld --user=mysql --bootstrap <<-EOF
     FLUSH PRIVILEGES;
     CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat ${MYSQL_PASSWORD_FILE})';
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-    SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$(cat ${MYSQL_ROOT_PASSWORD_FILE})');
+    SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
     FLUSH PRIVILEGES;
 EOF
 fi
